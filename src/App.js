a8f42f547f2ee19666346a3a5cefec45923ebde8
Irinel Bandas <ibandas@luc.edu>
2020-01-28 16:31:24 -0600

    filter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { render } from '@testing-library/react';
 import Products from './components/Products';
+import Filter from './components/Filter';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {products:[], filteredProducts: [], }
+    this.state = {products:[], filteredProducts: []};
+    this.handleChangeSort = this.handleChangeSort.bind(this);
   }
 
   componentWillMount(){
@@ -17,6 +17,22 @@ class App extends Component {
       filteredProducts: data
     })));
   }
+  handleChangeSort(e){
+    this.setState({sort: e.target.value});
+    this.listProducts();
+  }
+  listProducts(){
+    this.setState(state => {
+      if(state.sort !== ""){
+        state.products.sort((a,b)=>(state.sort==='lowest')?
+        (a.price > b.price?1:-1)
+        : (a.price < b.price?1:-1))
+      } else {
+        state.products.sort((a,b) => (a.id < b.id?1:-1));
+      }
+      return {filteredProducts: state.products};
+    })
+  }
   render() {
     return (
       <div className="container">
@@ -24,6 +40,9 @@ class App extends Component {
       <hr/>
       <div className="row">
         <div className="col-md-8">
+        <Filter size={this.state.size} sort={this.state.sort} handleChangeSize={this.handleChangeSize}
+        handleChangeSort={this.handleChangeSort} count={this.state.filteredProducts.length} />
+        <hr/>
           <Products products={this.state.filteredProducts} handleAddToCart={this.handleAddToCart}/>
         </div>
         <div className="col-md-8">
